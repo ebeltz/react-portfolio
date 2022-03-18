45622238e94ab70d3378984814a7632de7e2746a
ebeltz <ebbeltz@gmail.com>
2022-03-17 22:18:59 -0400

    added resume to resume page and updated skills

--- Main/src/components/Resume/index.js
+++ Main/src/components/Resume/index.js
@@ -5,7 +5,7 @@ function Resume() {
     <section className="my-5">
       <div className="my-2">
       <p>
-        Download my <a href="../../assets/Elizabeth Beltz Resume.pdf">resume</a>
+        Download my <a href="https://github.com/ebeltz/react-portfolio/blob/main/Main/src/assets/Elizabeth%20Beltz%20Resume.pdf">resume</a>
         </p>
         <h3>Front-end Proficiencies</h3>
         <ul className="skills">
@@ -13,9 +13,10 @@ function Resume() {
           <li>CSS</li>
           <li>JavaScript</li>
           <li>jQuery</li>
-          <li>responsive design</li>
+          <li>Responsive Design</li>
           <li>React</li>
           <li>Bootstrap</li>
+          <li>Handlebars</li>
         </ul>
         <h3>Back-end Proficiencies</h3>
         <ul className="skills">
@@ -26,6 +27,9 @@ function Resume() {
           <li>MongoDB, Mongoose</li>
           <li>REST</li>
           <li>GraphQL</li>
+          <li>MERN</li>
+          <li>Inquirer</li>
+          <li>NoSQL</li>
         </ul>
       </div>
     </section>
