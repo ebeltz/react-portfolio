e0ac86ffe0286c5881b8fb7e75adbc95e1bd653b
ebeltz <ebbeltz@gmail.com>
2022-03-17 21:54:23 -0400

    updated font and fixed navigation menu

--- Main/src/components/Header/index.js
+++ Main/src/components/Header/index.js
@@ -4,7 +4,7 @@ import coverImage from '../../assets/cover/cover-image.jpg';
 function Header(props) {
 
   return (
-    <header className="flex-row space-between px-1">
+    <header className="flex-row space-between px-2">
       <h1>Elizabeth Beltz</h1>
       <img src={coverImage} alt="snow covered mountain"></img>
       {props.children}
